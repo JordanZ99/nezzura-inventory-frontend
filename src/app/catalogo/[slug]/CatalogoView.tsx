fb0b7e8c8d970234964b358c9ebbfbbc4ac0f913
JordanZ99 <jordyz2005@gmail.com>
2026-08-14 18:40:59 -0500

    Revert "Catálogo: modal de producto con layout split estilo Instagram en desktop"
    This reverts commit 3c26693d5c7311e8c0cf489e2878722c86e71cf8.

--- src/app/catalogo/[slug]/CatalogoView.tsx
+++ src/app/catalogo/[slug]/CatalogoView.tsx
@@ -606,7 +606,6 @@ export default function CatalogoView({ slug }: { slug: string }) {
                     producto={productoActivo}
                     config={config}
                     tema={tema}
-                    esMovil={esMovil}
                     onClose={cerrarProducto}
                 />
             )}
